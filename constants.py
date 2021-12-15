e418b539f6cc3048462569807cd7664f346e775b
Reddersc022 <64492350+Reddersc022@users.noreply.github.com>
2021-12-15 20:42:18 +0000

    Add cooldowns

--- constants.py
+++ constants.py
@@ -2,3 +2,6 @@ HELP_MSG = (
     "Hi! I'm a bot made by @redders02#8850\n"
     "Detailed info can be found on my GitHub repo: https://github.com/Reddersc022/Discord_Bot or just ask redders!\n"
 )
+
+KILL_COOLDOWN = 60
+JOKE_COOLDOWN = 120
