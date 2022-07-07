c1a8eaa4daf4c795c66f05d72464769889ceb220
Charlie Redpath <64492350+ch0rl@users.noreply.github.com>
2022-07-07 15:14:32 +0100

    Remove puns

--- cogs/general.py
+++ cogs/general.py
@@ -23,31 +23,6 @@ class C_General(Cog):
         )
         print(f"Kill with !c kill {self.kill_token}")
 
-    @Cog.listener()
-    async def on_message(self, message: Message):
-        msg = message.content.lower()
-
-        # Jokes
-        if (
-            time.time() - self.last_joke > JOKE_COOLDOWN and
-            message.author != self.bot.user
-            ):
-            for i in ("i'm", "im", "i am"):
-                if i in msg:
-                    await message.reply(f"Hi {msg[msg.index(i)+len(i):]}, I'm a bot!")
-                    self.last_joke = time.time()
-                    break
-            else:
-                last_word = msg.split()[-1]
-                for c in ".!?)":
-                    last_word = last_word.replace(c, "")
-
-                if (
-                    not last_word == "her" and (last_word.endswith("er") or last_word.endswith("a"))
-                ):
-                    await message.reply(f"{last_word.title()}? I barely know her!")
-                    self.last_joke = time.time()
-
     @command()
     async def kill(self, ctx: Context):
         msg = ctx.message.content
