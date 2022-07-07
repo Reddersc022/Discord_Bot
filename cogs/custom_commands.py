056433565897afba3176fefd6d8220456d420bdf
Charlie Redpath <64492350+ch0rl@users.noreply.github.com>
2022-07-07 17:27:37 +0100

    fixes

--- cogs/custom_commands.py
+++ cogs/custom_commands.py
@@ -3,8 +3,8 @@
 import random
 import time
 import json
-from typing import Dict
 from discord import Message
+from typing import Dict
 from discord.ext.commands import Cog, command, Context, Bot
 from collections import defaultdict
 
@@ -16,31 +16,34 @@ class C_Custom_Commands(Cog):
         with open("commands.json") as f:
             self.commands: Dict[str, str] = json.load(f)
 
-    async def __format_reply(self, s: str, ctx: Message) -> str:
-        _msg = ctx.content.split()[1:]
+    async def __format_reply(self, s: str, m: Message) -> str:
+        _msg = m.content.split()[1:]
         return s.format(
             **{i: j for i, j in enumerate(_msg)},
             **{
-                "author": ctx.author.display_name,
+                "author": m.author.display_name,
             }
         )
 
     @Cog.listener()
-    async def on_message(self, ctx: Message):
-        if ctx.author != self.bot.user:
-            start = ctx.content.split()[0]
+    async def on_message(self, ctx: Context):
+        msg: Message = ctx.message
+        if msg.author != self.bot.user:
+            start = msg.content.split()[0]
             if start in self.commands.keys():
-                await ctx.channel.send(self.__format_reply(self.commands[start], ctx))
+                await ctx.channel.send(self.__format_reply(self.commands[start], msg))
 
     @command(aliases=("editcom",))
-    async def addcom(self, ctx: Message):
-        _msg = ctx.content.split()
+    async def addcom(self, ctx: Context):
+        msg: Message = ctx.message
+        _msg = msg.content.split()
         self.commands[_msg[0]] = _msg[1:]
 
     @command()
-    async def delcom(self, ctx: Message):
-        if ctx.content.split()[0] in self.commands:
-            del self.commands[ctx.content.split()[0]]
+    async def delcom(self, ctx: Context):
+        msg: Message = ctx.message
+        if msg.content.split()[0] in self.commands:
+            del self.commands[msg.content.split()[0]]
 
 
 def setup(bot: Bot):
