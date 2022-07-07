dc967b1cfd2954c1cea2c4c1a7912dce0aec3712
Charlie Redpath <64492350+ch0rl@users.noreply.github.com>
2022-07-07 17:20:26 +0100

    fixes

--- cogs/custom_commands.py
+++ cogs/custom_commands.py
@@ -17,7 +17,7 @@ class C_Custom_Commands(Cog):
             self.commands: Dict[str, str] = json.load(f)
 
     async def __format_reply(self, s: str, ctx: Message) -> str:
-        _msg = ctx.message.content.split()[1:]
+        _msg = ctx.content.split()[1:]
         return s.format(
             **{i: j for i, j in enumerate(_msg)},
             **{
@@ -27,18 +27,20 @@ class C_Custom_Commands(Cog):
 
     @Cog.listener()
     async def on_message(self, ctx: Message):
-        start = ctx.message.content.split()[0]
-        if start in self.commands.keys():
-            await ctx.channel.send(self.__format_reply(self.commands[start], ctx))
+        if ctx.author != self.bot.user:
+            start = ctx.content.split()[0]
+            if start in self.commands.keys():
+                await ctx.channel.send(self.__format_reply(self.commands[start], ctx))
 
     @command(aliases=("editcom",))
     async def addcom(self, ctx: Message):
-        self.commands[ctx.message.split()[0]] = ctx.message.split()[1:]
+        _msg = ctx.content.split()
+        self.commands[_msg[0]] = _msg[1:]
 
     @command()
     async def delcom(self, ctx: Message):
-        if ctx.message.split()[0] in self.commands:
-            del self.commands[ctx.message.split()[0]]
+        if ctx.content.split()[0] in self.commands:
+            del self.commands[ctx.content.split()[0]]
 
 
 def setup(bot: Bot):
